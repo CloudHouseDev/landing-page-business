b308f68986097982bfb9dd4b7b83e7ac233b7536
Luccas Specht <luccasspecht70@gmail.com>
2023-03-25 19:41:05 -0300

    wip: clean up project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DefaultPage from './pages/Default';
+
+function App() {
+  return <DefaultPage />;
+}
+
+export default App;
---
+++ src/pages/Default.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DefaultPage() {
+  return <h1>opa</h1>;
+}
+
+export default DefaultPage;
