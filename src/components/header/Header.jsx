8349bbb216e25220025e5636f60096255e78825b
Luccas Specht <luccasspecht70@gmail.com>
2023-03-26 14:41:44 -0300

    style: add hamburger button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useState } from "react";
 
 import CloudHouseIT from "../../assets/images/cloud_house_logo_transparent_dark.png";
 
 import "./styles.css";
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="header">
             <img src={CloudHouseIT} alt="Cloud House It logo" />
-            <ul>
-                <li>Home</li>
-                <li>Sobre Nós</li>
-                <li>Produtos e Serviços</li>
-                <li>Contato</li>
+            <ul className="wrapper-nav">
+                <li className="nav">Home</li>
+                <li className="nav">Sobre Nós</li>
+                <li className="nav">Produtos e Serviços</li>
+                <li className="nav">Contato</li>
             </ul>
+            <button
+                className={classNames("container", {
+                    change: isOpen,
+                })}
+                type="button"
+                onClick={() => setIsOpen((currentState) => !currentState)}
+            >
+                <div className="bar1" />
+                <div className="bar2" />
+                <div className="bar3" />
+            </button>
         </header>
     );
 }
