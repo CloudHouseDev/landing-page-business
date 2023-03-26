eef5c9e70e2a62e3ce9be4c184f920c694e4e36a
Luccas Specht <luccasspecht70@gmail.com>
2023-03-26 19:36:42 -0300

    refactor: footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,8 +5,8 @@ import "./styles.css";
 function Footer() {
     return (
         <footer className="footer">
-            <span>Todos os direitos reservados</span>
-            <strong>
+            <span>Copyright © 2023. Todos os direitos reservados</span>
+            <strong className="footer-details">
                 Cloud House IT,
                 <a
                     href="https://www.linkedin.com/in/gabriel-leite-676253189/"
