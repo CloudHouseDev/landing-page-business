ca01fc18aebf9fc6fe9766cca8e26e0729403445
Luccas Specht <luccasspecht70@gmail.com>
2023-03-31 18:59:26 -0300

    chore: form and infos of the contact section

--- src/pages/components/contactUs/contactUs.jsx
+++ src/pages/components/contactUs/contactUs.jsx
@@ -5,6 +5,7 @@ import {
     PhoneOutlined,
     MailOutlined,
     WhatsAppOutlined,
+    CaretRightOutlined,
 } from "@ant-design/icons";
 
 import SectionTitle from "../../../components/sectionTitle/sectionTitle";
@@ -16,7 +17,56 @@ function ContactUs() {
         <section className="wrapper-contact-us-section">
             <SectionTitle text="Contato" />
             <div className="content-wrapper-asides">
-                <aside>form</aside>
+                <form className="contact-infos">
+                    <div className="wrapper-inputs">
+                        <div>
+                            <label htmlFor="nameInput">Nome</label>
+                            <input
+                                type="text"
+                                placeholder="Ex.: Maria Antonia da Silva"
+                                id="nameInput"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="emailInput">E-mail</label>
+                            <input
+                                type="email"
+                                placeholder="Ex.: seuemail@gmail.com"
+                                id="emailInput"
+                            />
+                        </div>
+                    </div>
+                    <div className="wrapper-inputs">
+                        <div>
+                            <label htmlFor="subjectInput">Assunto</label>
+                            <input
+                                type="text"
+                                placeholder="Ex.: Site Corporativo"
+                                id="subjectInput"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="phoneNumberInput">Celular</label>
+                            <input
+                                type="text"
+                                placeholder="Ex.: (51) 99854-88190"
+                                id="phoneNumberInput"
+                            />
+                        </div>
+                    </div>
+                    <div className="wrapper-inputs">
+                        <div>
+                            <label htmlFor="messageInput">Mensagem</label>
+                            <textarea
+                                id="messageInput"
+                                placeholder="Digite sua mensagem aqui"
+                            />
+                        </div>
+                    </div>
+                    <button type="button" className="button-submit">
+                        Enviar <CaretRightOutlined />
+                    </button>
+                </form>
                 <aside className="personal-infos">
                     <div className="personal-info-details">
                         <EnvironmentOutlined />
@@ -33,19 +83,19 @@ function ContactUs() {
                         <MailOutlined />
                         <span>cloudhousegls@gmail.com</span>
                     </div>
-                    {/* <div className="wrapper-clickable-links">
-                        <div className="clickable-links">
+                    {/*   <div className="wrapper-clickable-links">
+                        <a className="clickable-links" href="opa">
                             <div>
                                 <WhatsAppOutlined />
                             </div>
                             <span>WhatsApp</span>
-                        </div>
-                        <div className="clickable-links">
+                        </a>
+                        <a className="clickable-links" href="opa">
                             <div>
                                 <MailOutlined />
                             </div>
                             <span>E-mail</span>
-                        </div>
+                        </a>
                     </div> */}
                 </aside>
             </div>
